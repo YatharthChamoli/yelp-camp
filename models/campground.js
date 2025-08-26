6b5c1da290f8b084bbb61ca099d9339ad88be12d
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2025-08-27 00:01:20 +0530

    make routes folder setup session and flash

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review')
 
 
 // This schema defines the structure of the campground documents in the MongoDB collection
@@ -8,7 +9,23 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,   
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    } 
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
